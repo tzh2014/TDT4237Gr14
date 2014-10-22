2032c6e8d3ab10bd631804f2c630fdb3b37f120f
Zehua Tian <tian2013nw@gmail.com>
2014-10-23 00:40:00 +0200

    fix user session timeout

--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@ session_cache_limiter(false);
 session_start();
 setcookie("PHPSESSID", session_id(), 0, "/", "", FALSE, TRUE);
 
-$timeout = 600; // Number of seconds until it times out.
+$timeout = 1200; // Number of seconds until it times out.
      
 // Check if the timeout field exists.
 if(isset($_SESSION['timeout'])) {
@@ -20,10 +20,7 @@ if(isset($_SESSION['timeout'])) {
     // visit is larger than the timeout period.
     $duration = time() - (int)$_SESSION['timeout'];
     if($duration > $timeout) {
-        // Destroy the session and restart it.
-        session_destroy();
-        session_start();
-        setcookie("PHPSESSID", session_id(), 0, "/", "", FALSE, TRUE);
+        unset($_SESSION['user']);
         }
     }
      
